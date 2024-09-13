6077a02e9cea35a7defacf4eaf924ee642708dcb
Aniket <aniketjadhav310@gmail.com>
2024-09-13 16:04:11 +0530

    responsive work done

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8g5_-odgx*vx(v2xfwx^&aaz5zmvalis$3gkh$r730=(6px9r%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','192.168.1.85']
 
 
 # Application definition
